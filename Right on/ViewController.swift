61cc22d993b0804899f41b6e4e5cc07fbb9f7aa9
Pavel <pavel.818@yandex.ru>
2022-06-22 13:35:04 +0400

    Initial Commit

---
+++ Right on/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Right on
+//
+//  Created by Pavel Абрамов on 22.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
